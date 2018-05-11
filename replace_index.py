93dfb07f20636eaa76f40efb865f0e8775365638
yuki-nguyen <38753169+yuki-nguyen@users.noreply.github.com>
2018-05-11 11:11:30 +0700

    Complete_ Resolve issue
    Complete_ Resolve issue

--- replace_index.py
+++ replace_index.py
@@ -1,4 +1,4 @@
-
+import os
 def inplace_change(filename, old_string, new_string):
     # Safely read the input filename using 'with'
     with open(filename) as f:
@@ -13,26 +13,34 @@ def inplace_change(filename, old_string, new_string):
         s = s.replace(old_string, new_string)
         f.write(s)
 file_name = '/home/tuyet/Git/shell/test.txt'
-
 file_new = open('/home/tuyet/Git/shell/name_ID.txt', 'r')
 
-replaces = file_new.readlines()
+replaces = file_new.read().splitlines()
 
 file_old = open(file_name, 'r')
+contents = file_old.read().splitlines()
 
-contents = file_old.readline()
+for content in contents:
+    content_values = content.split('=')
+    name = content_values[0]
+    id = content_values[1]
 
-for replace in replaces:
-    replace_values = replace.split('=')
-    name_replace = replace_values[0]
-    id_replace = replace_values[1]
-    for content in contents:
-        content_values = content.split('=')
-        name = content_values[0]
-        id = content_values[1]
+    for replace in replaces:
 
-        if (name == name_replace) :
+        replace_values = replace.split('=')
+        name_replace = replace_values[0]
+        # print(name_replace)
+        id_replace = replace_values[1]
+        # print(id_replace)
+
+        if ((name == name_replace) and (id != id_replace)):
             inplace_change(file_name, content, replace)
-        if name_replace not in file_old:
-            file_old.append("{} = {}".format(name_replace, id_replace))
+
+        with open(file_name, "r+") as f:
+            line_found = any(replace in line for line in f)
+            if not line_found:
+                f.seek(0, os.SEEK_END)
+                f.write("{}\n".format(replace))
+
+
 

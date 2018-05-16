550805d58ca131963a63c0816b786670c45c805a
yuki-nguyen <38753169+yuki-nguyen@users.noreply.github.com>
2018-05-16 10:22:54 +0700

    Change for loop to while loop
    Change for loop to while loop for reading line in file

--- append_log_id.sh
+++ append_log_id.sh
@@ -6,7 +6,7 @@
 
 file_url="/home/tuyet/Git/file_url.txt"
 
-for origin_url in $(cat $file_url) # Đọc file này để đọc từng hàng trong file, mỗi câu lệnh git clone là 1 giá trị
+while read -r origin_url # Đọc file này để đọc từng hàng trong file, mỗi câu lệnh git clone là 1 giá trị
 
 do 
 
@@ -58,4 +58,4 @@ for line in $(cat $file_log)
 		fi
 	  done
 done
-done 
+done < "$file_url"

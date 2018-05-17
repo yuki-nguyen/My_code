8d3bb89a24182990abb83bdf8ca4f3799452619c
yuki-nguyen <38753169+yuki-nguyen@users.noreply.github.com>
2018-05-17 15:14:55 +0700

    Do nothing when we can't clone
    Do nothing when we can't clone (add continue to if condition when we check success of clone command)

--- append_log_id.sh
+++ append_log_id.sh
@@ -5,12 +5,12 @@
 path="/home/tuyet/Git" # Đường dẫn tới thư mục mà tại đó ta thực hiện lệnh git clone
 
 File_for_customer="$path/For_customer.txt" # f
-rm -rf $File_for_customer 
+rm -rf $File_for_customer
 
 # Ý tưởng nếu để 1 đống url vào 1 file, mỗi hàng là 1 câu lệnh git clone url
 
 file_url="/home/tuyet/Git/file_url.txt"
-sed -i '/^$/d' $file_url # Xóa tất cả các dòng trắng có tồn tại trong file 
+sed -i '/^$/d' $file_url
 
 while IFS= read -r origin_url|| [ -n "$origin_url" ];  # Thay cho while read -r origin_url # Đọc file này để đọc từng hàng trong file, mỗi câu lệnh git clone là 1 giá trị
 
@@ -43,25 +43,28 @@ if [[ $success -eq 0 ]]; then
 		echo "Repository $name_project successfully cloned."
 	else
         	echo "Something went wrong clone $name_project, clone fail!"
-		continue
+        	continue
         fi
 cd $name_project
 git log --oneline --pretty=format:%h -n 1 > $file_log_ID # ghi ID của project vào đây
 git log --pretty=format:'%h %<(20)%an %s'| awk -F '|' '{ printf "%s %-20s %s\n", $1, $2, $3 }' > temp_log.txt
-id=$(cat $file_log_ID) # id mới nhất vừa clone về
+
+id_root=`grep "$name_project=" $file_log | cut -d"=" -f2` # id làm mốc để in từ mới nhất tới id này
+
 echo "This is information for $name_project:" >> $File_for_customer
 while read -r line_temp
 	do
 	id_old=`echo $line_temp|cut -d " " -f1`
-	if [[ $id_old != $id ]]; then
+	if [[ $id_old != $id_root ]]; then
 	      echo $line_temp >> $File_for_customer
 	else
-	       echo "$id_old is nearest id that you commit" >> $File_for_customer
+	       # echo "$id_old is nearest id that you commit" >> $File_for_customer
+	       echo $line_temp >> $File_for_customer
 	       break
     fi
 
 done <"temp_log.txt"
-
+id=$(cat $file_log_ID)
 for line in $(cat $file_log)
 	do
 	  NAME_PROJECT=$(echo $line|awk -F '=' '{print $1}') # Lấy name của project trong file đối chiếu
